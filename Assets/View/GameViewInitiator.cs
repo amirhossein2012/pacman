57ed94aeaed5f11f5af0fe8d794873b332a2dc10
amir <amir@gmail.com>
2017-04-08 16:35:29 +0430

    ok

--- Assets/View/GameViewInitiator.cs
+++ Assets/View/GameViewInitiator.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameViewInitiator : MonoBehaviour {
-
+   
 	// Use this for initialization
 	void Start () {
         GameInitiator.Start();
-	}
+       
+
+    }
+    
 
 }
