9ad54d94488d0e0b3caf2bea7b301444330e2a1a
amir <amir@gmail.com>
2017-04-08 17:45:18 +0430

    view has bugs

--- Assets/View/GameViewInitiator.cs
+++ Assets/View/GameViewInitiator.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 public class GameViewInitiator : MonoBehaviour {
-
+    GameObject sprite;
+    public Sprite sp;
     // Use this for initialization
     void Start()
     {
-
-
-
-        GameInitiator.Start();
-
+       
+        sprite = new GameObject();
+        sprite.AddComponent<SpriteRenderer>();
+        sprite.GetComponent<SpriteRenderer>().sprite = sp;
     }
 
 }
