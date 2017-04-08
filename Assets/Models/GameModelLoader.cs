0510934cb9a1bd5f0f2e0472ff4007892203f5ae
amir <amir@gmail.com>
2017-04-08 14:10:02 +0430

    GameLoader updated

--- Assets/Models/GameModelLoader.cs
+++ Assets/Models/GameModelLoader.cs
@@ -5,24 +5,38 @@ using System.IO;
 using System.Xml;
 public class GameModelLoader : MonoBehaviour {
 
-
-    static int row;
-    
-    // Use this for initialization
+    //Get data from xml file
+    static int row,col,square_size,wall_density;
     void Start()
     {
         XmlDocument doc = new XmlDocument();
         doc.Load("Assets\\Models\\GameModel.xml");
 
-        //Display all the book titles.
-        
+        //setting tags
         XmlNodeList elemList = doc.GetElementsByTagName("row");
-       
         foreach(XmlNode x in elemList)
         {
             row =XmlConvert.ToInt32( x.InnerText);
         }
-        print(row);
+        elemList = doc.GetElementsByTagName("col");
+
+        foreach (XmlNode x in elemList)
+        {
+            col = XmlConvert.ToInt32(x.InnerText);
+        }
+        elemList = doc.GetElementsByTagName("squaresize");
+
+        foreach (XmlNode x in elemList)
+        {
+            square_size = XmlConvert.ToInt32(x.InnerText);
+        }
+        elemList = doc.GetElementsByTagName("wallDensity");
+       
+        foreach(XmlNode x in elemList)
+        {
+            wall_density =XmlConvert.ToInt32( x.InnerText);
+        }
+       
 
     }
 }
