e00346ad03e11856daa72996c8f27b894dac6e18
amir <amir@gmail.com>
2017-04-08 13:26:23 +0430

    xmlloader

---
+++ Assets/Models/GameModelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+public class GameModelLoader : MonoBehaviour {
+
+
+    static int row;
+    
+    // Use this for initialization
+    void Start()
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load("Assets\\Models\\GameModel.xml");
+
+        //Display all the book titles.
+        
+        XmlNodeList elemList = doc.GetElementsByTagName("row");
+       
+        foreach(XmlNode x in elemList)
+        {
+            row =XmlConvert.ToInt32( x.InnerText);
+        }
+        print(row);
+
+    }
+}
